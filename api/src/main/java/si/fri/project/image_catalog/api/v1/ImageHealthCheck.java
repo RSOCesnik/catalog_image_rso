8ff75a876737a16e4a0d4a2b0922d4045eece7e3
cesnik <cesnik@localhost.localdomain>
2020-01-04 20:25:39 +0100

    fixed healthchecks

---
+++ api/src/main/java/si/fri/project/image_catalog/api/v1/ImageHealthCheck.java
@@ -0,0 +1,24 @@
+//package si.fri.project.image_catalog.api.v1;
+//
+//import org.eclipse.microprofile.health.Health;
+//import org.eclipse.microprofile.health.HealthCheck;
+//import org.eclipse.microprofile.health.HealthCheckResponse;
+//import si.fri.project.image_catalog.services.AppProperties;
+//
+//import javax.enterprise.context.ApplicationScoped;
+//import javax.inject.Inject;
+//
+//@Health
+//@ApplicationScoped
+//public class ImageHealthCheck implements HealthCheck {
+//    @Inject
+//    private AppProperties appProperties;
+//
+//    @Override
+//    public HealthCheckResponse call() {
+//        if(appProperties.isHealthy()) {
+//            return HealthCheckResponse.named(ImageHealthCheck.class.getSimpleName()).up().build();
+//        }
+//        return HealthCheckResponse.named(ImageHealthCheck.class.getSimpleName()).down().build();
+//    }
+//}
