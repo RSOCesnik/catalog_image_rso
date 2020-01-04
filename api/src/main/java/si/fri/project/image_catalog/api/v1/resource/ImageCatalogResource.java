791892b85aa35e0b2b9263d06ee1bd469ab91b29
cesnik <cesnik@localhost.localdomain>
2020-01-04 11:51:21 +0100

    remove

--- api/src/main/java/si/fri/project/image_catalog/api/v1/resource/ImageCatalogResource.java
+++ api/src/main/java/si/fri/project/image_catalog/api/v1/resource/ImageCatalogResource.java
@@ -26,24 +26,11 @@ public class ImageCatalogResource {
     @Inject
     private ImageBean imageBean;
 
-//    @GET
-//    public Response getComments() {
-//        return Response.ok("CATALOG API").build();
-//    }
     @GET
     public Response getPhotos() {
         List<ImageEntity> photos = imageBean.getPhotos(uriInfo);
 
         return Response.ok(photos).build();
-    }
-    @GET
-    @Path("/info")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response printInfo() {
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("opis_projekta", "S pomocjo nasega projekta, lahko dodajate slike, jih gledate ali komentirate");
-        return Response.ok(map).build();
     }
 
 }
